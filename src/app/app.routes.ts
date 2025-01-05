cb5e1ffef1af479545bb235248de9582898b48c2
Living Room <livingroom@Living-Room-Mac-Mini.local>
2025-01-05 13:24:36 -0600

    cube init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { RootComponent } from './root/root.component';
 import { AuthComponent } from './auth/auth.component';
+import { CubeComponent } from './cube/cube.component';
 
 export const routes: Routes = [
     {path: 'auth', component: AuthComponent},
+    {path: 'cube', component: CubeComponent},
     {path: 'root', component: RootComponent},
     {path: '**', component: RootComponent},
 ];
---
+++ src/app/cube/cube.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cube',
+  imports: [],
+  templateUrl: './cube.component.html',
+  styleUrl: './cube.component.css'
+})
+export class CubeComponent {
+
+}
