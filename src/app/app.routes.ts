dd7396fdbf0009af87664aafe889d9cabf930523
Michael Simpson <michaelsimpson@Michaels-MacBook-Air.local>
2025-01-25 07:44:40 -0600

    add resume

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RootComponent } from './root/root.component';
 import { AuthComponent } from './auth/auth.component';
 import { CubeComponent } from './cube/cube.component';
 import { WebsocketComponent } from './websocket/websocket.component';
+import { ResumeComponent } from './resume/resume.component';
 
 export const routes: Routes = [
     {path: 'auth', component: AuthComponent},
     {path: 'cube', component: CubeComponent},
     {path: 'root', component: RootComponent},
     {path: 'websocket', component: WebsocketComponent},
+    {path: 'resume', component: ResumeComponent},
     {path: '**', component: RootComponent},
 ];
