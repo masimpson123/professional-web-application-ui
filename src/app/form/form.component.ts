f2a14a8d7397e928da2ec4968eeef16a428fec57
michaelsimpson <masimpson123@gmail.com>
2025-12-09 11:38:33 -0600

    form 1

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [ReactiveFormsModule, AsyncPipe, JsonPipe],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
   standalone: true
 })
 export class FormComponent {
-
+  securityForm = new FormGroup({
+    securityTerm: new FormControl(':)'),
+    securityToken: new FormControl('jwt-1234'),
+  });
+onSubmit() {
+    console.log(this.securityForm.value);
+  }
 }
