80f3be6a07e32e8a457573fb1632a38aaf40d5d4
michaelsimpson <masimpson123@gmail.com>
2025-12-11 12:12:58 -0600

    form experience

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
-import { AsyncPipe, JsonPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { ConfirmationComponent } from './confirmation/confirmation.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { UserIntakeComponent } from './user-intake/user-intake.component';
@@ -8,12 +8,13 @@ import { SecurityIntakeComponent } from './security-intake/security-intake.compo
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule, AsyncPipe, JsonPipe, ConfirmationComponent, WelcomeComponent, UserIntakeComponent, SecurityIntakeComponent],
+  imports: [NgIf, ReactiveFormsModule, AsyncPipe, JsonPipe, ConfirmationComponent, WelcomeComponent, UserIntakeComponent, SecurityIntakeComponent],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css',
   standalone: true
 })
 export class FormComponent {
+  currentStep = 1;
   securityForm = new FormGroup({
     userIntake: new FormGroup({
       usersName: new FormControl('Mike'),
@@ -24,7 +25,15 @@ export class FormComponent {
       securityToken: new FormControl('jwt-1234')
     })
   });
-onSubmit() {
+  onSubmit() {
     console.log(this.securityForm.value);
+  }
+  next() {
+    if ( this.currentStep === 4 ) return;
+    this.currentStep++;
+  }
+  previous() {
+    if ( this.currentStep === 1 ) return;
+    this.currentStep--;
   }
 }
