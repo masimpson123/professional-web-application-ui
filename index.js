33f43bbb8468a3b99105c62de5689f56b755a370
michaelsimpson <masimpson123@gmail.com>
2026-02-14 13:17:14 -0600

    all routes update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static("dist/client-2026/browser"));
 
-app.get('*', function(req, res) {
+app.get('/*all-routes', function(req, res) {
    res.sendFile(__dirname + '/dist/client-2026/browser/index.html');
 });
 
