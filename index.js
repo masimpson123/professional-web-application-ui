f6777c571c5306cd2bf91fa9496985af496887fb
michaelsimpson <masimpson123@gmail.com>
2026-02-16 11:43:19 -0600

    update routes for tensor flow

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var app = express();
 
 app.use(express.static("dist/client-2026/browser"));
 
-app.get('/*all-routes', function(req, res) {
-   res.sendFile(__dirname + '/dist/client-2026/browser/index.html');
+// TODO: remove after next successful deployment
+// app.get('/*all-routes', function(req, res) {
+//    res.sendFile(__dirname + '/dist/client-2026/browser/index.html');
+// });
+
+app.get('/tensorflow', function(req, res) {
+   res.send('this route will serve tensor flow based machine learning predictions');
 });
 
 app.listen(8080);
