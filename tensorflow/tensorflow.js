7685e8ae3f6126673088b3b2db73eec66e9988e3
michaelsimpson <masimpson123@gmail.com>
2026-02-26 17:15:32 -0600

    comment

--- tensorflow/tensorflow.js
+++ tensorflow/tensorflow.js
@@ -32,7 +32,7 @@ async function trainModel(trainingData) {
   if (!trainingData.length) throw new Error('No training data!');
   const model = tf.sequential();
   model.add(tf.layers.dense({inputShape: [1], units: 1, useBias: true})); // input
-  model.add(tf.layers.dense({units: 50, activation: 'relu'})); // hidden
+  model.add(tf.layers.dense({units: 50, activation: 'relu'})); // hidden with Rectified Linear Unit (ReLU) activation
   model.add(tf.layers.dense({units: 1, useBias: true})); // output
 
   const {inputs, labels} = getTensors(trainingData);
